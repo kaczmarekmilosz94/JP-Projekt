10354ee9d4b529c77edf4c86b1c39c4d97763c0f
kaczmarekmilosz94 <46039679+kaczmarekmilosz94@users.noreply.github.com>
2019-06-13 20:32:33 +0200

    Update core.c
    Some corrections + comments

--- core.c
+++ core.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <SDL2/SDL.h>
 
-#pragma warning(disable:4996)
+#pragma warning(disable:4996) //Disables warning about safety for windows when using printf instead of printf_s
 
 #undef main
 
@@ -52,21 +52,20 @@ typedef struct Planet {
 }planet;
 
 
-planet main_planets[10];
 
-static double G = 0.6726;
+planet main_planets[10];   //array of objects (planets) used in simulation
 
-static double dt = 0.1f;
+static double G = 0.6674;  // Gravitational constant, should be (6.674...) * 10^-11
+static double dt = 0.1f;   // Time step, precision of caltualation depends on it
+double t = 0;              // Current time of simulation (not the real time)
+double duration = 10;      // Duration of simulation
 
-const int map_size_x = 50;
-const int map_size_y = 100;
 
-char map[50][100];
 
-double t = 0;
-double duration = 10;
 
-int PollEventsForQuit() {
+
+//makes window do not freeze during the simulation
+int PollEventsForQuit() { 
 	SDL_Event e;
 
 	while (SDL_PollEvent(&e))
@@ -82,8 +81,8 @@ int PollEventsForQuit() {
 	return 0;
 }
 
-void DrawCircle(SDL_Renderer * renderer, int a, int b, double r, color color)
-{
+//a simple funtion I wrote to draw circle (uses (x-a)^2 + (y-b)^2 = r^2)
+void DrawCircle(SDL_Renderer * renderer, int a, int b, double r, color color) {
 	for (int i = -r; i < r; i += r / 5)
 	{
 		SDL_Rect srcrect;
@@ -126,7 +125,7 @@ void DrawCircle(SDL_Renderer * renderer, int a, int b, double r, color color)
 	}
 }
 
-
+// Euler algorytm
 planet Euler(planet planet, force force)
 {
 	double ax = force.x / planet.mass;
@@ -141,6 +140,7 @@ planet Euler(planet planet, force force)
 	return planet;
 }
 
+// Calculate gravity force for every planet in the environment of other planets
 force Gravity(planet planet1, planet planets[10], int size)
 {
 	force final_force = { 0,0 };
@@ -201,40 +201,47 @@ force Gravity(planet planet1, planet planets[10], int size)
 	return final_force;
 }
 
+
 void PrintPosition(planet planets[10], int size, SDL_Renderer * renderer, int tracer)
 {
-	system("@cls||clear");
+	// ++++++++++++++ Printing stats in console ++++++++++++++
+
+	system("@cls||clear"); // clear console every "frame"
 
 	int i, j;
 
 	printf("TIME: %0.2f \n", t);
 
-
-
-
-
-
-	for (i = 0; i < size; i++)
-	{
+	for (i = 0; i < size; i++)	{
 		printf("%s:    Mass: %0.1f    X: %f    Y: %f      Vx: %f    Vy: %f\n",
 			planets[i].name, planets[i].mass, planets[i].position.x, planets[i].position.y, planets[i].velocity.x, planets[i].velocity.y);
 	}
 
 	printf("\n");
 
+	// ++++++++++++++ END Printing stats in console ++++++++++++++
 
-	color black = { 0,0,0,255 };
 
+	// ++++++++++++++ Draw simulation ++++++++++++++
+
+	// set backcround color
+	color black = { 0,0,0,255 };                           
+
+	// if trace is off it clears render every frame
 	if (tracer == 0) {
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);    
 		SDL_RenderClear(renderer);
 	}
 
-
-	for (i = 0; i < size; i++)
-	{
+	// Draw object with given stats
+	for (i = 0; i < size; i++)	{
 		DrawCircle(renderer, (int)planets[i].position.x, (int)planets[i].position.y, (int)planets[i].radius, planets[i].color);
 	}
+
+	// ++++++++++++++ END Draw simulation ++++++++++++++
+
+
+	// ++++++++++++++ Save result ++++++++++++++
 
 	if (t >= duration)
 	{
@@ -247,6 +254,7 @@ void PrintPosition(planet planets[10], int size, SDL_Renderer * renderer, int tr
 		fprintf(fp, "DURATION: %f\n\n", duration);
 		fprintf(fp, "     Name     ||   Position   ||   Velocity   ||    Mass    ||    Collisions\n");
 
+		// Save object stats
 		for (i = 0; i < size; i++)
 		{
 			fprintf(fp, "   %s           %0.1f, %0.1f     %0.1f, %0.1f        %0.1f     %d",
@@ -256,52 +264,62 @@ void PrintPosition(planet planets[10], int size, SDL_Renderer * renderer, int tr
 				planets[i].mass,
 				planets[i].collisions_count);
 
-			if (planets[i].collisions_count > 0)
-			{
+
+			// Adds if object had any collisions 
+			if (planets[i].collisions_count > 0) {
+
 				int k;
 
-				for (k = 0; k < planets[i].collisions_count; k++)
-				{
+				for (k = 0; k < planets[i].collisions_count; k++) {
 					fprintf(fp, " %s(in %0.2f) | ", planets[i].collisions[k].name, planets[i].collisions[k].time);
 				}
 				fprintf(fp, "\n");
 			}
-			else
-			{
+
+			else {
 				fprintf(fp, "\n");
 			}
-		}
-
-
-
+		}		
 		fclose(fp);
 
-		exit(0);
+		exit(0); // close program when finished
 	}
+	// ++++++++++++++ END Save result ++++++++++++++
+
 }
 
-planet* SetMovement(planet * planets, int size, SDL_Renderer * renderer, int tracer)
+void SetMovement(planet * planets, int size, SDL_Renderer * renderer, int tracer)
 {
-	planet current_planets[10];
+	// makes copy of current planet status
+	// Thanks to this every planet can have its statistics calculated 
+	// for the exactly same moment
 
+	planet current_planets[10]; 
 	int i;
 
-	for (i = 0; i < size; i++)
-	{
+	for (i = 0; i < size; i++)	{
 		current_planets[i] = planets[i];
 	}
+	/////////////////////////////////////////////////////////////////
 
-	for (i = 0; i < size; i++)
-	{
-		planets[i] = Euler(planets[i], Gravity(planets[i], current_planets, size));
+
+
+	// ++++++++++++++ Calculate statistics for every planet ++++++++++++++
+
+	for (i = 0; i < size; i++)	{
+		planets[i] = Euler(                                
+			planets[i], 
+			Gravity(planets[i], current_planets, size));  
 	}
+	// ++++++++++++++ END Calculate statistics for every planet ++++++++++++++
 
+
+
+	// Print stats in console and draw objects
 	PrintPosition(planets, size, renderer, tracer);
 
+	// Set current time
 	t += dt;
-
-
-	return planets;
 }
 
 
@@ -309,15 +327,16 @@ void Controller(planet * planets, int size, SDL_Renderer * renderer, int tracer)
 {
 	while (1==1)
 	{
-		if (PollEventsForQuit()) break;
+		if (PollEventsForQuit()) break; //makes windows do not freeze during the simulation
 
-		planets = SetMovement(planets, size, renderer, tracer);
+		SetMovement(planets, size, renderer, tracer);
 	}
 }
 
 
-planet GetPlanet(char setts[1024])
+planet GetPlanet(char setts[1024]) //GetPlanet() is used to get object settings from string
 {
+	//All needed keys to "create object"
 	char name[15];
 	double x;
 	double y;
@@ -329,15 +348,16 @@ planet GetPlanet(char setts[1024])
 	int g;
 	int b;
 	int a;
+	////////////////////////////////////
+
+	
+	// ++++++++++++++ Get values from string  ++++++++++++++
+
+	char delim[] = ",";
+	char *ptr = strtok(setts, delim);     // Split string by ','
 
 	int i = 0;
-
-	int init_size = strlen(setts);
-	char delim[] = ",";
-
-	char *ptr = strtok(setts, delim);
-
-	while (ptr != NULL)
+	while (ptr != NULL)                   // Attach splited values to keys 
 	{
 		printf("'%s'\n", ptr);
 		ptr = strtok(NULL, delim);
@@ -356,7 +376,10 @@ planet GetPlanet(char setts[1024])
 
 		i++;
 	}
+	// ++++++++++++++ END Get values from string ++++++++++++++
 
+
+	// ++++++++++++++ Create object ++++++++++++++
 
 	position pos = { x,y };
 	velocity vel = { vx,vy };
@@ -371,64 +394,63 @@ planet GetPlanet(char setts[1024])
 	_planet.radius = radius;
 	_planet.collisions_count = 0;
 
+	// ++++++++++++++ END Create object ++++++++++++++
+
 	return _planet;
 }
 
 
 int main()
 {
-	printf("Start\n");
+	// ++++++++++++++ Reading confing file ++++++++++++++
 
-	char ch, file_name[25];
+	char file_name[25];
 	FILE *fp;
 
-	int tracer = 1;
-	int size = 2;
-
-	int id = 0;
-
+	int tracer; // drawing orbit on=1 / off=0
+	int size;   // how many objects from array should the simulator use
+	int id = 0; // id of current added object (position of object in array)
 
 	printf("Enter name of a file you wish to see\n");
+	printf("or write 'test.txt' to use the example\n");
 	gets(file_name);
 
 	fp = fopen(file_name, "r"); // read mode
 
-	if (fp == NULL)
-	{
+	if (fp == NULL) {
 		perror("Error while opening the file.\n");
 		exit(EXIT_FAILURE);
 	}
 
 
-	char x[1024];
-	int key_id = 0;
-	/* assumes no word exceeds length of 1023 */
-	while (fscanf(fp, " %1023s", x) == 1)
-	{
-		switch (key_id)
+	char x[1024]; //variable that contains splited words from file
+	int key_id = 0; //used to check what kind of value program is reading now <key><value>
+
+	while (fscanf(fp, " %1023s", x) == 1) {
+		switch (key_id) //default 0 
 		{
-		case 1:
+		case 1: // SET size 
 			printf("size: %s\n", x);
 			size = atoi(x);
 			key_id = 0;
 			break;
-		case 2:
+		case 2: // SET duration
 			printf("duration: %s\n", x);
 			duration = atof(x);
 			key_id = 0;
 			break;
-		case 3:
+		case 3: // SET orbit on/off
 			printf("trace: %s\n", x);
 			tracer = atoi(x);
 			key_id = 0;
 			break;
-		case 4:
+		case 4: // SET objects
 			printf("object: %s\n", x);
-			main_planets[id] = GetPlanet(x);
+			main_planets[id] = GetPlanet(x); //GetPlanet() is used to get object settings from string
 			id++;
 			key_id = 0;
 			break;
-		case 5:
+		case 5: // SET delta time / time step
 			printf("step: %s\n", x);
 			dt = atof(x);
 			key_id = 0;
@@ -437,17 +459,20 @@ int main()
 			break;
 		}
 
+		// sets int for key_id,that means next word is value of current key
+
 		if (strcmp(x, "size:") == 0) key_id = 1;
 		if (strcmp(x, "duration:") == 0) key_id = 2;
 		if (strcmp(x, "trace:") == 0) key_id = 3;
 		if (strcmp(x, "object:") == 0) key_id = 4;
 		if (strcmp(x, "step:") == 0) key_id = 5;
 	}
-
-
 	fclose(fp);
 
-	while (1 == 0) {}
+	//++++++++++++++ END Reading file ++++++++++++++
+
+
+	//++++++++++++++ Set sdl setting for creating graphics ++++++++++++++
 
 	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_EVENTS) != 0) {
 		return -1;
@@ -464,33 +489,32 @@ int main()
 	if (renderer == NULL) {
 		SDL_DestroyWindow(win);
 		SDL_Quit();
-
 		return -1;
 	}
-
-	for (int i = 0; i < map_size_x; i++)
-	{
-		for (int j = 0; j < map_size_y; j++)
-		{
-			map[i][j] = ' ';
-		}
-	}
+	//++++++++++++++ END Set sdl setting for creating graphics ++++++++++++++
 
 
+	//++++++++++++++ Start infinite loop main funcition ++++++++++++++
 
 	while (1 == 1) {
 
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 		SDL_RenderClear(renderer);
 
-
-		Controller(main_planets, size, renderer, tracer, duration);
-
+		Controller(main_planets, size, renderer, tracer);	//Controller initialize main functions using setting from file
 	}
+
+	//++++++++++++++ END Start infinite loop main funcition ++++++++++++++
+
+
+	//++++++++++++++ Clear sdl when finished ++++++++++++++
 
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(win);
 	SDL_Quit();
+
+	//++++++++++++++ END Clear sdl when finished ++++++++++++++
+
 
 	return 0;
 }
